f721238ed639b312ebe8f1e37a440f803ea0b834
slowice <448220654@qq.com>
2026-04-27 20:13:34 +0800

    1

---
+++ src/main/java/com/ccr/CcrApplication.java
@@ -0,0 +1,11 @@
+package com.ccr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcrApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CcrApplication.class, args);
+    }
+}
